d0099b51e37d74a6d9a9e1e333deda6b8a5fac6a
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-03-21 20:57:15 -0600

    Comit Inicial

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Usuario;
+
+class UsuarioController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        $usuario = new Usuario;
+         
+        /*$usuario->tipoUsuario = $request->tipoUsuario;
+        $usuario->apellidoP = $request->apellidoP;
+        $usuario->apellidoM = $request->apellidoM;
+        $usuario->email = $request->email;
+        $usuario->contrasena = $request->contrasena;
+        $usuario->fechaNac = $request->fechaNac;
+        $usuario->telefono = $request->telefono;
+        $usuario->calificacion = $request->calificacion;
+        $usuario->fotoPerfil = $request->fotoPerfil;*/
+        echo $usuario;
+        
+        return $usuario->save();
+
+    ]);   
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        return Usuario::findOrFail($id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+
+}
