6bf6dacfaa48c5afcea575a47ff5c6b0e032f762
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-03-23 12:12:51 -0600

    Obtencion de el objeto usuario con todos los campos desde la api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,14 +48,15 @@ class AuthController extends Controller
             $token->expires_at = Carbon::now()->addWeeks(1);
         }
         $token->save();
-        return response()->json([
+		return response()->json([
                     'access_token' => $tokenResult->accessToken,
                     'token_type' => 'Bearer',
                     'expires_at' => Carbon::parse(
                                     $tokenResult->token->expires_at)
                             ->toDateTimeString(),
+                    'usuario' => $user,
         ]);
-    }
+	}
     
     public function logout(Request $request)
     {
