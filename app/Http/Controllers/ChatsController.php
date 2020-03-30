b9feb77985d95ec093faede9e429e51a58db49f4
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-03-29 22:08:34 -0600

    Se agrego ID a la respuesta de chats

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -39,7 +39,7 @@ class ChatsController extends Controller
         $user = $request->user();
        $chats =DB::table('chats')
                 ->join('users', 'chats.id_proveedor', '=', 'users.id')
-                ->select('users.email', 'users.name')
+                ->select('users.id','users.email', 'users.name')
                 ->where('id_usuario', $user["id"])
                 ->get();
 
