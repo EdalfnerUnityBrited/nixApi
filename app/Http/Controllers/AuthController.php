b1bd5b358e493ec268d39d90a44a047b0fb3e451
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-05-16 21:05:21 -0500

    A ver

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Laravel\Cashier\Cashier;
 
 class AuthController extends Controller
 {
