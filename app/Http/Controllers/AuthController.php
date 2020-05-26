1616ba3202f711e6b1e8c1b63f6c75170f78ab82
EdalfnerUnityBrited <60933236+EdalfnerUnityBrited@users.noreply.github.com>
2020-05-26 08:38:23 -0500

    Arreglado metodos de pago

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -154,7 +154,7 @@ class AuthController extends Controller
 
         $user=$request->user();
         $stripeCharge = $user->charge(10000, $request->input('paymentMethodId'),[
-            "description": 'Prueba de mensaje para stripe',
+            "description"=> 'Prueba de mensaje para stripe'
         ]);
         return response()->json(['message'=>'Successfully added card!']);
     
